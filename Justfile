0923fb76f101a40dcb812e2c0d93ffb0e9232946
Tyler Michael Smith <tysmith@redhat.com>
2025-06-09 14:14:35 +0000

    Update, simplify
    Signed-off-by: Tyler Michael Smith <tysmith@redhat.com>

--- Justfile
+++
@@ -1,7 +0,0 @@
-KN := "kubectl -n $NAMESPACE"
-NAMESPACE := "vllm-clusterscale"
-
-
-exec:
-  kubectl cp run.sh {{NAMESPACE}}/ibgda-repro:/app/run.sh \
-  && kubectl exec -it ibgda-repro -- /bin/bash
--- run.sh
+++
@@ -1,9 +0,0 @@
-export NVSHMEM_IB_ENABLE_IBGDA=1
-
-# Try again
-mpirun --allow-run-as-root\
-       -np 2 --bind-to none \
-       -x CUDA_VISIBLE_DEVICES=0,1 \
-       -x NVSHMEM_BOOTSTRAP=MPI \
-       /tmp/nvshmem_src/build/perftest/device/coll/barrier_latency -i 1000
-
